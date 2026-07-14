e54c656ed2e9926605bc8e270c724613f8d59437
Brighware <brigh@brighware.com>
2026-07-14 14:26:34 -0400

    Updated webpage

--- front/website/src/main.js
+++ front/website/src/main.js
@@ -16,8 +16,93 @@ import App from './App.vue'
 // Styles
 import 'unfonts.css'
 
+import formidable from 'formidable';
+import stream from 'node:stream';
+
 const app = createApp(App)
 
 registerPlugins(app)
 
 app.mount('#app')
+
+var uploaded_binary;
+var uploaded_file_path;
+var uploaded_file_size;
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+
+/** @param {import('formidable').File} file */
+function fileWriteStreamHandler(file) {
+  // TODO
+}
+
+/** @param {Event} event */
+function handleSubmit(event) {
+ /** @type {HTMLFormElement} */
+  const form = event.currentTarget;
+  const url = new URL(form.action);
+  const formData = new FormData(form);
+  const searchParams = new URLSearchParams(formData);
+
+  /** @type {Parameters<fetch>[1]} */
+  const fetchOptions = {
+    method: form.method,
+  };
+
+  if (form.method.toLowerCase() === 'post') {
+    if (form.enctype === 'multipart/form-data') {
+      fetchOptions.body = formData;
+    } else {
+      fetchOptions.body = searchParams;
+    }
+  } else {
+    url.search = searchParams;
+  }
+
+  fetch(url, fetchOptions);
+
+  event.preventDefault();
+}
+
+/**
+ * @see https://nuxt.com/docs/guide/concepts/server-engine
+ * @see https://github.com/unjs/h3
+ */
+export default defineEventHandler(async (event) => {
+  let body;
+  const headers = getRequestHeaders(event);
+
+  if (headers['content-type']?.includes('multipart/form-data')) {
+    body = await parseMultipartNodeRequest(event.node.req);
+  } else {
+    body = await readBody(event);
+  }
+  console.log(body);
+  uploaded_binary = body;
+
+  return { ok: true };
+});
+
+/**
+ * @param {import('http').IncomingMessage} req
+ */
+function parseMultipartNodeRequest(req) {
+  return new Promise((resolve, reject) => {
+    /** @see https://github.com/node-formidable/formidable/ */
+    const form = formidable({
+      multiples: true,
+      fileWriteStreamHandler: fileWriteStreamHandler,
+    })
+    form.parse(req, (error, fields, files) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve({ ...fields, ...files });
+    });
+  });
+}
+
+
